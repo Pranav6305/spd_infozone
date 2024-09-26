6e2b7bd8077152d3f94426565fe6d3e695fd71db
Pranav6305 <pranavji6305@gmail.com>
2024-09-26 13:50:03 +0530

    database connected successfully

--- members/models.py
+++ members/models.py
@@ -3,11 +3,22 @@
 from django.db import models
 
 class Student(models.Model):
-    roll_no = models.CharField(max_length=10)
+    roll_no = models.CharField(max_length=255, primary_key=True)
     name = models.CharField(max_length=100)
     department = models.CharField(max_length=100, default="Unknown Department")  # Add a default value here
     address = models.CharField(max_length=255, default="No address provided")  # Keep your previous default
     contact_no = models.CharField(max_length=20, null=True, blank=True)
 
-    def __str__(self):
-        return self.name
+#     def __str__(self):
+#         return self.name
+
+# class Student(models.Model):
+    # roll_no = models.CharField(max_length=255, primary_key=True)  # Explicitly set as primary key
+#     name = models.CharField(max_length=255)
+#     department = models.CharField(max_length=255)
+#     address = models.CharField(max_length=255)
+#     contact_no = models.CharField(max_length=15)
+
+    class Meta:
+        db_table = 'members_student'  # Ensure this points to the correct table name in MySQL
+
